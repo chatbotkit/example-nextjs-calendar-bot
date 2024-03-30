2c331d21245997f49dc230c1f28a504ba428d64f
pdparchitect <pdp@gnucitizen.org>
2024-03-30 13:01:29 +0000

    UI improvements.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import { complete } from '@/actions/conversation'
 import ChatArea from '@/components/ChatArea'
+import Footer from '@/components/Footer'
 import '@/components/functions'
 
 import ConversationManager from '@chatbotkit/react/components/ConversationManager'
@@ -8,43 +9,13 @@ export default function Page() {
   return (
     <ConversationManager endpoint={complete}>
       <div className="mx-auto w-full max-w-2xl py-20 space-y-6">
-        <p>
-          Please use the chat area below to book an appointment with me. My
+        <p className="text-lg text-gray-700">
+          Please use the area below to book an appointment with me. My
           intelligent assistant will help you with selecting the right time and
           date for your appointment.
         </p>
         <ChatArea />
-        <p className="text-center text-gray-500 text-sm">
-          Made with ❤️ by{' '}
-          <a
-            href="https://chatbotkit.com"
-            target="_blank"
-            className="text-blue-500"
-          >
-            ChatBotKit
-          </a>
-        </p>
-        <p className="text-center text-gray-500 text-sm">
-          <a
-            className="flex flex-row justify-center items-center gap-2 text-blue-500"
-            href="https://github.com/chatbotkit/example-nextjs-calendar-bot"
-            target="_blank"
-            title="Fork this example on GitHub"
-          >
-            <svg
-              className="w-[1em] h-[1em]"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.86 2.33.66.07-.52.28-.86.51-1.06-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-              />
-            </svg>
-            <span>Fork on GitHub</span>
-          </a>
-        </p>
+        <Footer />
       </div>
     </ConversationManager>
   )
--- components/ChatArea.jsx
+++ components/ChatArea.jsx
@@ -49,52 +49,47 @@ export default function ChatArea() {
 
   return (
     <div className="rounded-lg border text-card-foreground shadow-sm bg-gray-100 p-4 flex flex-col gap-4">
-      <div className="flex flex-col gap-4">
-        {messages
-          .filter(({ type }) => ['user', 'bot'].includes(type))
-          .map(({ id, type, text, children }, index, messages) => {
-            const disableInteractivity = index < messages.length - 1
+      {messages.length ? (
+        <div className="flex flex-col gap-4">
+          {messages
+            .filter(({ type }) => ['user', 'bot'].includes(type))
+            .map(({ id, type, text, children }, index, messages) => {
+              const disableInteractivity = index < messages.length - 1
 
-            switch (type) {
-              case 'user':
-                return <UserMessage key={id} text={text} />
+              switch (type) {
+                case 'user':
+                  return <UserMessage key={id} text={text} />
 
-              case 'bot':
-                return (
-                  <BotMessage key={id} text={text}>
-                    {children ? (
-                      <div
-                        className={clsx(
-                          'transition-all duration-300 ease-in-out w-full max-w-md',
-                          {
-                            'opacity-40 pointer-events-none':
-                              disableInteractivity,
-                          }
-                        )}
-                      >
-                        {children}
-                      </div>
-                    ) : null}
-                  </BotMessage>
-                )
-            }
-          })}
-        {message ? <BotMessage key={message.id} text={message.text} /> : null}
-        {thinking ? <BotMessage key="thinking" text="● ● ●" /> : null}
-      </div>
+                case 'bot':
+                  return (
+                    <BotMessage key={id} text={text}>
+                      {children ? (
+                        <div
+                          className={clsx(
+                            'transition-all duration-300 ease-in-out w-full max-w-md',
+                            {
+                              'opacity-40 pointer-events-none':
+                                disableInteractivity,
+                            }
+                          )}
+                        >
+                          {children}
+                        </div>
+                      ) : null}
+                    </BotMessage>
+                  )
+              }
+            })}
+          {message ? <BotMessage key={message.id} text={message.text} /> : null}
+          {thinking ? <BotMessage key="thinking" text="● ● ●" /> : null}
+        </div>
+      ) : null}
       <ChatInput
-        className="rounded-md border border-gray-100 bg-white px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+        className="rounded-md border border-gray-100 bg-white px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none resize-none"
         value={text}
         onChange={(e) => setText(e.target.value)}
         onSubmit={submit}
         placeholder="Your message..."
-        style={{
-          border: 0,
-          outline: 'none',
-          resize: 'none',
-          width: '100%',
-          marginTop: '10px',
-        }}
       />
     </div>
   )
