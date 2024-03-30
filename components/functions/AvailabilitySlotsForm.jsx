8b1f239e651e33a7879b28ad7965181102fa5e08
pdparchitect <pdp@gnucitizen.org>
2024-03-30 09:21:52 +0000

    Changes.

--- components/functions/AvailabilitySlotsForm.jsx
+++ components/functions/AvailabilitySlotsForm.jsx
@@ -8,23 +8,19 @@ export default function AvailabilitySlotsForm({ slots }) {
   const { request } = useContext(ConversationContext)
 
   function captureSlot(slot) {
-    request('captureSlot', slot)
+    request('captureSlot', { slot })
   }
 
   return (
     <form className="flex flex-row flex-wrap gap-2">
-      {slots.map((slot, index) => {
-        const day = slot.day
-        const time = slot.time
-        const duration = slot.duration
-
+      {slots.map(({ slot }, index) => {
         return (
           <div
             key={index}
             className="bg-white hover:shadow-md hover:transform hover:-translate-y-0.5 rounded-xl p-4 cursor-pointer transition-all duration-300 ease-in-out"
             onClick={captureSlot.bind(null, slot)}
           >
-            {day} at {time} for {duration} minutes
+            {slot}
           </div>
         )
       })}
